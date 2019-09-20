e7f9b498f2002cd1b4b1067dda72a9f0dd1c4ad8
Shirom Makkad <shirmak11@gmail.com>
2019-09-20 16:54:38 -0400

    Added fzf to the setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sudo apt-get install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 sudo apt-get update
