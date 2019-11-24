00cdc8d9d1c60bfb2fbb0588906ba0e39093cf54
Shirom Makkad <shirmak11@gmail.com>
2019-11-24 15:23:14 -0500

    Updated vim to latest version

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@ sudo apt-get install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
+sudo apt update
+sudo apt install vim
