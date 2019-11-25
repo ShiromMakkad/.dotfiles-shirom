0002179f07231b3e2c659ec757aaa0b1073fee3a
Shirom Makkad <shirmak11@gmail.com>
2019-11-25 17:29:13 -0500

    Another approach to loading tmuxline on install

--- post-git-clone.sh
+++ post-git-clone.sh
@@ -1,7 +1,8 @@
-sudo chmod +x setup.sh
-./setup.sh
 cd ~
 cp -a .dotfiles-shirom/. ~
+sudo chmod +x setup.sh
+./setup.sh
+git clone https://github.com/edkolev/tmuxline.vim.git ~/.vim/bundle/tmuxline.vim && rm -r ~/.vim/bundle/tmuxline.vim/.git
 rm "Install instructions.txt"
 rm "p10k config.txt"
 rm setup.sh
