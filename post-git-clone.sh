2a2da19c21fd56197f815cc83301c3888172b0aa
Shirom Makkad <shirom@Shirom-PC.localdomain>
2019-11-27 14:13:03 -0600

    Changed RAM message to work on other computers

--- post-git-clone.sh
+++ post-git-clone.sh
@@ -6,7 +6,7 @@ git clone https://github.com/edkolev/tmuxline.vim.git ~/.vim/bundle/tmuxline.vim
 rm "Install instructions.txt"
 rm "p10k config.txt"
 rm setup.sh
-rm "Inconsolata Nerd Font Complete Windows Compatible.otf"
+rm "Inconsolata Nerd Font Complete Mono Windows Compatible.otf"
 rm -r .git
 rm -r .dotfiles-shirom
 rm -r "Windows Terminal"
