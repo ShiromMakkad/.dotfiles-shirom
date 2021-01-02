ab04cd4aee137c04ff53791e693f1e814967651e
Shirom Makkad <shirmak11@gmail.com>
2021-01-02 01:34:13 -0600

    Added curl to install script

--- os/Ubuntu/setup.sh
+++ os/Ubuntu/setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
+sudo apt-get install curl
 sudo apt-get install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
