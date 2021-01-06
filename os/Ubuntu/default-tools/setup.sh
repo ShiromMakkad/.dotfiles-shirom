e792536cbf26b1ad009edf111eafed738599f13d
Shirom Makkad <shirmak11@gmail.com>
2021-01-06 23:23:50 +0000

    Updated anaconda install script

--- os/Ubuntu/default-tools/setup.sh
+++ os/Ubuntu/default-tools/setup.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 # anaconda
-if command -v COMMAND &> /dev/null; then
+if ! command -v conda &> /dev/null; then
     wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
     bash Anaconda3-2020.11-Linux-x86_64.sh
     rm Anaconda3-2020.11-Linux-x86_64.sh
