40d38bad15f8c6e2c98400531cbe451e85d7be37
ShiromMakkad <shirmak11@gmail.com>
2023-08-07 23:14:19 -0400

    Made Fedora more universal

--- os/Fedora/setup.sh
+++ os/Fedora/setup.sh
@@ -1,9 +1,9 @@
-sudo dnf upgrade
-sudo dnf install zsh
+sudo yum upgrade
+sudo yum install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sudo dnf install tmux
-sudo dnf install sysstat
-sudo dnf install autojump-zsh
-sudo dnf install neovim
+sudo yum install tmux
+sudo yum install sysstat
+sudo yum install autojump-zsh
+sudo yum install neovim
