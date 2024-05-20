ed023de1736aafd771d79443516f5a62da41d0d4
ShiromMakkad <shirmak11@gmail.com>
2024-05-20 01:28:04 -0400

    Add helix installation

--- os/Mac/setup.sh
+++ os/Mac/setup.sh
@@ -3,19 +3,11 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 brew update
 
-read -p "Are you on an M1 mac? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	arch -arm64 brew install fzf
-	arch -arm64 brew install tmux
-	arch -arm64 brew install autojump
-else
-	brew install fzf
-	brew install tmux
-	brew install autojump
-fi
+brew install fzf
+brew install tmux
+brew install autojump
 brew install neovim
+brew install helix
 
 $(brew --prefix)/opt/fzf/install
 
--- os/Ubuntu/setup.sh
+++ os/Ubuntu/setup.sh
@@ -5,3 +5,6 @@ sudo apt-get install tmux
 sudo apt-get install sysstat
 sudo apt-get install autojump
 sudo apt-get install neovim
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt-get update
+sudo apt-get install helix
